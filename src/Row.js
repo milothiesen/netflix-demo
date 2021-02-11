6ec843771fe976c9f2ebcfe4217b10da8d10754c
Thiesen <mailethiesen@gmail.com>
2021-02-11 05:48:12 -0500

    added edge cases for movie title for the trailer search

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,14 @@
 import './Row.css';
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 
 const base_url = 'https://image.tmdb.org/t/p/original/';
 
 function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState('');
 
     useEffect(() => {
         // run once when row loads
@@ -19,6 +22,31 @@ function Row({ title, fetchUrl, isLargeRow }) {
         fetchData();
     }, [fetchUrl]);
 
+    const opts = {
+        height: '390',
+        width: '100%',
+        origin: 'http://localhost:3000',
+        playerVars: {
+            // https://developers.google.com/youtube/player_parameters,
+            autoplay: 1,
+        },
+    };
+
+    const handleClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl('');
+        } else {
+            movieTrailer(
+                movie?.title || movie?.name || movie?.original_name || ''
+            )
+                .then((url) => {
+                    const urlParams = new URLSearchParams(new URL(url).search);
+                    setTrailerUrl(urlParams.get('v'));
+                })
+                .catch((error) => console.log(error));
+        }
+    };
+
     return (
         <div className='row'>
             <h2>{title}</h2>
@@ -28,16 +56,23 @@ function Row({ title, fetchUrl, isLargeRow }) {
                     <img
                         // adding key here boosts performance
                         key={movie.id}
+                        onClick={() => handleClick(movie)}
                         className={`row_poster ${
                             isLargeRow && 'row_posterLarge'
                         }`}
                         src={`${base_url}${
                             isLargeRow ? movie.poster_path : movie.backdrop_path
                         }`}
-                        alt={movie.name}
+                        alt={
+                            movie?.title ||
+                            movie?.name ||
+                            movie?.original_name ||
+                            ''
+                        }
                     />
                 ))}
             </div>
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
 
         // {/* container -> posters */}
