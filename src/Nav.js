2ce121ee9e2ed858b7cd1d484bbded8c32748482
Thiesen <mailethiesen@gmail.com>
2021-02-11 04:34:01 -0500

    adding nav bar styling

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,24 @@
 import './Nav.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Nav() {
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            } else {
+                handleShow(false);
+            }
+        });
+        return () => {
+            window.removeEventListener('scroll');
+        };
+    }, []);
+
     return (
-        <div className='nav'>
+        <div className={`nav ${show && 'nav_black'}`}>
             <img
                 className='nav_logo'
                 src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png'
